fb794aab425007873955184791333d17bc235c22
TamoorIqbal <tamooriqbal37@gmail.com>
2023-08-09 14:55:00 +0500

    fixed ui

--- src/App.js
+++ src/App.js
@@ -1,143 +1,145 @@
-import React from 'react'
-import { makeStyles, withStyles } from '@material-ui/core/styles'
-import Accordion from '@material-ui/core/Accordion'
-import AccordionDetails from '@material-ui/core/AccordionDetails'
-import AccordionSummary from '@material-ui/core/AccordionSummary'
-import Typography from '@material-ui/core/Typography'
-import ExpandMoreIcon from '@material-ui/icons/ExpandLess'
-import { Box, Button, Container, MenuItem, Select } from '@material-ui/core'
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandLess";
+import { Box, Button, Container, MenuItem, Select } from "@material-ui/core";
 
-import Badge from '@material-ui/core/Badge'
-import AddCircleIcon from '@material-ui/icons/AddCircle'
-import { itemsArray } from './DummyData'
-import CustomizedProgressBars from './ProgressBar'
+import Badge from "@material-ui/core/Badge";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import { itemsArray } from "./DummyData";
+import CustomizedProgressBars from "./progressbar";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%'
+    width: "100%",
   },
   heading: {
-    fontSize: '9.6px',
-    fontWeight: '700',
-    alignItems: 'center',
-    display: 'flex',
-    color: 'black',
-    marginLeft: '.5rem'
+    fontSize: theme.typography.pxToRem(10),
+    fontWeight: 700,
+    alignItems: "center",
+    display: "flex",
+    color: "black",
+    marginLeft: ".5rem",
   },
   quantity: {
-    fontSize: '9.6px',
+    fontSize: "9.6px",
     // fontWeight: 900,
     // color: "black",
     // paddingTop: ".2rem",
-    color: 'red'
+
+    color: "red",
   },
   customAccordion: {
-    position: 'relative'
+    position: "relative",
   },
   customExpandIcon: {
-    position: 'absolute',
-    right: 4,
-    top: -2,
-    color: 'black'
+    // position: "absolute",
+    padding: " 7px 8px 8px 0px",
+    // right: '15px',
+    // top: '-5px',
+    color: "black",
   },
   parentHeading: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '95%',
-    color: 'black'
+    display: "flex",
+    justifyContent: "space-between",
+    width: "95%",
+    color: "black",
   },
   subHeading: {
     fontWeight: 400,
-    fontSize: '13px',
-    color: 'black'
+    fontSize: "13px",
+    color: "black",
   },
   AddNote: {
     fontWeight: 400,
     fontSize: theme.typography.pxToRem(11),
-    color: 'black'
+    color: "black",
   },
   paragraph: {
-    fontWeight: 300,
-    fontSize: '13.44px',
-    width: '90%',
-    color: 'black'
+    fontWeight: 400,
+    fontSize: "13.44px",
+    width: "90%",
+    color: "black",
   },
   footerBadge: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '100%',
-    alignItems: 'center'
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+    alignItems: "center",
   },
   footerText: {
-    color: 'red',
-    fontSize: '9.6px'
+    color: "red",
+    fontSize: "9.6px",
   },
   trade: {
     fontSize: theme.typography.pxToRem(9.29),
     fontWeight: 700,
-    color: 'black'
+    color: "black",
   },
   Mep: {
-    fontSize: theme.typography.pxToRem(1)
+    fontSize: theme.typography.pxToRem(1),
   },
   sameItems: {
     fontSize: theme.typography.pxToRem(11),
-    color: 'black',
-    fontWeight: 700
+    color: "black",
+    fontWeight: 700,
   },
   sameItemsValue: {
     fontSize: theme.typography.pxToRem(10),
-    color: 'black',
-    fontWeight: 600
+    color: "black",
+    fontWeight: 700,
   },
   ItemHeading: {
-    fontSize: theme.typography.pxToRem(9.29),
-    fontWeight: '700',
-    alignItems: 'center',
-    display: 'flex',
-    color: 'black',
-    paddingTop: '.2rem'
+    fontSize: theme.typography.pxToRem(10),
+    fontWeight: 700,
+    alignItems: "center",
+    display: "flex",
+    color: "black",
+    paddingTop: ".2rem",
   },
   selectComponent: {
     fontWeight: 600,
-    color: '#4CB6D9',
-    fontSize: theme.typography.pxToRem(11)
+    color: "#4CB6D9",
+    fontSize: theme.typography.pxToRem(11),
   },
   addButton: {
     fontWeight: 700,
     height: theme.typography.pxToRem(13),
-    fontSize: theme.typography.pxToRem(13),
-    color: '#4CB6D9',
-    background: '#EDEDED',
-    padding: '12px 5px',
-    marginLeft: '5px',
-    marginBottom: '5px'
+    fontSize: theme.typography.pxToRem(10),
+    color: "#4CB6D9",
+    background: "#EDEDED",
+    padding: "12px 5px",
+    marginLeft: "5px",
+    marginBottom: "5px",
   },
   Muiselect: {
-    outline: 'none'
+    outline: "none",
   },
   progressBar: {
-    height: '75%',
+    height: "75%",
     width: 12,
     borderRadius: 50,
-    flexDirection: 'column-reverse',
-    backgroundColor: 'rgba(255, 255, 255)'
+    flexDirection: "column-reverse",
+    backgroundColor: "rgba(255, 255, 255)",
   },
   selectBox: {
-    width: '100%',
+    width: "100%",
     height: 50,
-    fontWeight: 600,
-    color: '#4CB6D9',
-    backgroundColor: '#F7F7F7',
-    borderRadius: '5px',
-    paddingLeft: '15px',
-    boxShadow: 'none',
-    fontSize: theme.typography.pxToRem(12)
+    fontWeight: 700,
+    color: "#4CB6D9",
+    backgroundColor: "#F7F7F7",
+    borderRadius: "5px",
+    paddingLeft: "15px",
+    boxShadow: "none",
+    fontSize: theme.typography.pxToRem(11),
   },
   menuItem: {
-    fontSize: theme.typography.pxToRem(12)
-  }
-}))
+    fontSize: theme.typography.pxToRem(10),
+  },
+}));
 
 // const StyledBadge = withStyles(theme => ({
 //   badge: {
@@ -147,42 +149,47 @@ const useStyles = makeStyles(theme => ({
 //     // padding: '0.5rem'
 //   }
 // }))(Badge)
-const StyledINTERIORBadge = withStyles(theme => ({
+const StyledINTERIORBadge = withStyles((theme) => ({
   badge: {
     // marginTop: ".1rem",
-    minWidth: '.9rem',
-    height: '.9rem',
-    backgroundColor: '#4CB6D9'
-  }
-}))(Badge)
+    minWidth: ".9rem !important",
+    height: ".9rem !important",
+    backgroundColor: "#4CB6D9 !important",
+  },
+}))(Badge);
 
 export default function App() {
-  const classes = useStyles()
-  const [expanded, setExpanded] = React.useState(false)
-  const [toggle, setToggle] = React.useState(true)
-  const [selectStatus, setSelectStatus] = React.useState('')
-  const [index, setIndex] = React.useState()
-  const handleStatusChange = event => {
-    setSelectStatus(event.target.value)
-  }
-  const handleChange = panel => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false)
-  }
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const [toggle, setToggle] = React.useState(true);
+  const [selectStatus, setSelectStatus] = React.useState("");
+  const [index, setIndex] = React.useState(-1);
+  const handleStatusChange = (event) => {
+    setSelectStatus(event.target.value);
+  };
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
   const handleButtonClick = () => {
     // Handle button click here
-  }
+  };
 
-  const togleHandler = (index, toggle) => {
+  const togleHandler = (_index, toggle) => {
     // alert('Please select')(
-    if (toggle) {
-      setIndex(index)
+    // if (toggle) {
+    //   setIndex(index);
+    // } else {
+    //   setIndex(-1);
+    // }
+    // setToggle(!toggle);
+    if (index === _index) {
+      setIndex(-1);
     } else {
-      setIndex(-1)
+      setIndex(_index);
     }
-    setToggle(!toggle)
-  }
+  };
 
-  console.log(index, 'index')
+  console.log(index, "index");
 
   return (
     <Container maxWidth="sm">
@@ -195,7 +202,7 @@ export default function App() {
             productDescription,
             quantity,
             status,
-            trade
+            trade,
           },
           _index
         ) => (
@@ -209,7 +216,7 @@ export default function App() {
                 classes={{ expandIcon: classes.customExpandIcon }}
                 onClick={() => togleHandler(_index, toggle)}
                 style={{
-                  transition: '1s'
+                  transition: "1s",
                 }}
                 expandIcon={<ExpandMoreIcon />}
                 aria-controls="panel1bh-content"
@@ -218,16 +225,16 @@ export default function App() {
                 {index !== _index && (
                   <Box
                     style={{
-                      width: '100%'
+                      width: "100%",
                     }}
                   >
                     <Box className={classes.parentHeading}>
                       <Box
                         style={{
-                          display: 'flex',
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                          textAlign: 'center'
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          textAlign: "center",
                         }}
                       >
                         <StyledINTERIORBadge color="secondary" variant="dot" />
@@ -263,7 +270,7 @@ export default function App() {
               <AccordionDetails>
                 <Box
                   style={{
-                    width: '100%'
+                    width: "100%",
                   }}
                 >
                   <Box>
@@ -274,8 +281,8 @@ export default function App() {
                     <Box>
                       <Box
                         style={{
-                          display: 'flex',
-                          justifyContent: 'space-between'
+                          display: "flex",
+                          justifyContent: "space-between",
                         }}
                       >
                         <Box>
@@ -305,7 +312,7 @@ export default function App() {
                           or around corner from driveway(or as otherwise
                           directed). Code set to Front door,car port door ,
                           utility room deadbolt. exterior storage room keyed
-                          knob. Mounted lock box code 1850.{' '}
+                          knob. Mounted lock box code 1850.{" "}
                         </Typography>
                       </Box>
                     </Box>
@@ -324,19 +331,19 @@ export default function App() {
 
                     <Select
                       className={classes.selectBox}
-                      inputProps={{ 'aria-label': 'Without label' }}
+                      inputProps={{ "aria-label": "Without label" }}
                       value={selectStatus}
                       onChange={handleStatusChange}
                       displayEmpty
-                      renderValue={value => (value === '' ? 'Select' : value)}
+                      renderValue={(value) => (value === "" ? "Select" : value)}
                     >
                       <MenuItem className={classes.menuItem} value="">
                         Select
                       </MenuItem>
-                      <MenuItem className={classes.menuItem} value={'active'}>
+                      <MenuItem className={classes.menuItem} value={"active"}>
                         Active
                       </MenuItem>
-                      <MenuItem className={classes.menuItem} value={'inactive'}>
+                      <MenuItem className={classes.menuItem} value={"inactive"}>
                         Inactive
                       </MenuItem>
                     </Select>
@@ -357,29 +364,29 @@ export default function App() {
                     </Box> */}
                     <Box
                       style={{
-                        margin: '8px 6px'
+                        margin: "8px 6px",
                       }}
                     >
                       <Typography
                         className={classes.heading}
                         style={{
-                          marginLeft: '0'
+                          marginLeft: "0",
                         }}
                       >
                         ADDITIONAL COMMENTS
                       </Typography>
                       <Box
                         style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          gap: '15px'
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "15px",
                         }}
                       >
                         <AddCircleIcon
                           onClick={handleButtonClick}
                           style={{
-                            color: '#4CB6D9',
-                            cursor: 'pointer'
+                            color: "#4CB6D9",
+                            cursor: "pointer",
                           }}
                         />
                         <Typography className={classes.AddNote}>
@@ -390,16 +397,16 @@ export default function App() {
                     <Typography className={classes.heading}>PHOTOS</Typography>
                     <Box
                       style={{
-                        width: '100%',
+                        width: "100%",
                         height: 50,
-                        borderRadius: '5px',
-                        backgroundColor: '#F7F7F7',
-                        padding: '3px 10px'
+                        borderRadius: "5px",
+                        backgroundColor: "#F7F7F7",
+                        padding: "3px 10px",
                       }}
                     >
                       <Typography
-                        style={{ paddingLeft: '8px' }}
-                        className={classes.paragraph}
+                        style={{ paddingLeft: "8px" }}
+                        className={classes.AddNote}
                       >
                         Photo 1
                       </Typography>
@@ -413,5 +420,5 @@ export default function App() {
         )
       )}
     </Container>
-  )
+  );
 }
